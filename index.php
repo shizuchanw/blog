987213cdc05550be181bf1e2a763457f61957203
shizuchanw <50822201+shizuchanw@users.noreply.github.com>
2021-03-04 17:55:44 +0800

    Add files via upload

---
+++ index.php
@@ -0,0 +1,28 @@
+#!/usr/local/bin/php
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="utf-8">
+	<title>Flora Wang</title>
+</head>
+<body>
+	<header>
+		<h1>Flora Wang</h1>
+	</header>
+	<main>
+		<section>
+			<h2>Tech, Business, Music</h2>
+			<h2>I wish to make this world a better place :)</h2>
+		</section>
+		<section>
+			<p>About</p>
+			<p>Posts</p>
+			<p>Email: florawang0611@gmail.com</p>
+		</section>
+	</main>
+	<footer>
+		<hr>
+		<small>&copy; Flora Wang, 2021</small>
+	</footer>
+</body>
+</html>
